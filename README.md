425b67e11b667becc91e9f0bdcedfba963e68910
inavamsi <inav65.com>
2021-02-26 00:33:55 +0530

    default paramter update

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Efficacy of Contact Tracing
 Finding the optimal sweet spot for contact tracing in a region.
 
+Play with the UI tool online at : https://contact-tracing.herokuapp.com
+
 ## Run
 To run locally
 
--- main.py
+++ main.py
@@ -76,20 +76,27 @@ def average(tdict,number):
 	return tdict
 
 def histogram():
+	st.write("""
+	# Efficacy of Contact Tracing
+	Finding the optimal sweet spot for contact tracing in a region.
+	""")
+	st.write("For any queries please email ibe214@nyu.edu")
+	st.write("------------------------------------------------------------------------------------")
+
 	st.sidebar.write("World parameters")
 	n=st.sidebar.slider("Number of agents", min_value=250 , max_value=3000 , value=1000 , step=250)
-	p=st.sidebar.slider("Probability(p) of an edge in G(n,p) random graph", min_value=0.0 , max_value=1.0 , value=0.01 , step=0.01 , format=None , key=None )
-	p_range=st.sidebar.checkbox("Divide p by 10")
+	p=st.sidebar.slider("Probability(p) of an edge in G(n,p) random graph", min_value=0.0 , max_value=1.0 , value=0.26 , step=0.01 , format=None , key=None )
+	p_range=st.sidebar.checkbox("Divide p by 10",value=True)
 	if p_range:
 		p/=10
-	days=st.sidebar.slider("Number of days in simulation", min_value=1 , max_value=300 , value=30 , step=1 , format=None , key=None )
-	num_worlds=st.sidebar.slider("Number of times to average simulations over", min_value=1 , max_value=100 , value=3 , step=1 , format=None , key=None )
+	days=st.sidebar.slider("Number of days in simulation", min_value=1 , max_value=300 , value=100 , step=1 , format=None , key=None )
+	num_worlds=st.sidebar.slider("Number of times to average simulations over", min_value=1 , max_value=100 , value=15 , step=1 , format=None , key=None )
 	st.sidebar.write("Averaging simulation "+str(num_worlds)+" times over graph G("+str(n)+","+str(p)+") for "+str(days)+" days.")
 	
 	st.sidebar.write("--------------")
 	st.sidebar.write("Contact Tracing parameters")
 	error_CT=st.sidebar.slider("Error proportion in identifying contacts", min_value=0.0 , max_value=1.0 , value=0.0 , step=0.01 , format=None , key=None )
-	max_degree=st.sidebar.slider("Degree range", min_value=0 , max_value=10 , value=3 , step=1 , format=None , key=None )
+	max_degree=st.sidebar.slider("Degree range", min_value=0 , max_value=10 , value=4 , step=1 , format=None , key=None )
 	qdegree_list=[]
 	for i in range(max_degree+1):
 		qdegree_list.append(i)
@@ -97,7 +104,7 @@ def histogram():
 	st.sidebar.write("--------------")
 
 	st.sidebar.write("Disease parameters")
-	num_exp=p=st.sidebar.slider("Starting exposed proportion", min_value=0.0 , max_value=1.0 , value=0.01 , step=0.01 , format=None , key=None )
+	num_exp=st.sidebar.slider("Starting exposed proportion", min_value=0.0 , max_value=1.0 , value=0.01 , step=0.01 , format=None , key=None )
 	beta_sy=st.sidebar.slider("Rate of infection due to Symptomatic : Susceptible->Exposed", min_value=0.0 , max_value=1.0 , value=0.3 , step=0.01 , format=None , key=None )
 	beta_asy=st.sidebar.slider("Rate of infection due to Asymptomatic : Susceptible->Exposed", min_value=0.0 , max_value=1.0 , value=0.3 , step=0.01 , format=None , key=None )
 	mu_sy=st.sidebar.slider("Rate of Exposed->Symptomatic", min_value=0.0 , max_value=1.0 , value=0.3 , step=0.01 , format=None , key=None )
@@ -122,19 +129,23 @@ def histogram():
 			for k in range(len(qlist)):
 				hdict['Quarantined'][qdegree]+=qlist[k]
 
+	for qdegree in qdegree_list:
+		for key in hdict.keys():
+			hdict[key][qdegree]/=num_worlds
+
 	labels=qdegree_list
 	x = np.arange(len(labels))  # the label locations
 	width = 0.2  # the width of the bars
 
 	fig, ax = plt.subplots()
-	rects1 = ax.bar(x-2*width, hdict['Symptomatic'], width, label='Symptomatic')
-	rects2 = ax.bar(x-width, hdict['Asymptomatic'], width, label='Asymptomatic')
-	rects4 = ax.bar(x, hdict['Quarantined'], width, label='Quarantined')
+	rects1 = ax.bar(x-width, hdict['Symptomatic'], width, label='Symptomatic')
+	rects2 = ax.bar(x, hdict['Asymptomatic'], width, label='Asymptomatic')
+	rects4 = ax.bar(x+width, hdict['Quarantined'], width, label='Quarantined')
 
 	# Add some text for labels, title and custom x-axis tick labels, etc.
 	ax.set_ylabel('Cumulative Hours')
 	ax.set_xlabel('Quarantine degree')
-	ax.set_title('Effects of different degrees of quarantine on G('+str(n)+','+str(p)+')')
+	ax.set_title('Costs of different degrees of quarantine on G('+str(n)+','+str(p)+')')
 	ax.set_xticks(x)
 	ax.set_xticklabels(labels)
 	ax.legend()
@@ -143,16 +154,39 @@ def histogram():
 
 	st.pyplot(fig)
 
+	st.write("------------------------------------------------------------------------------------")
 
-st.write("""
-# Efficacy of Contact Tracing
-Finding the optimal sweet spot for contact tracing in a region.
-""")
-st.write("For any queries please email ibe214@nyu.edu")
-st.write("------------------------------------------------------------------------------------")
+	st.header("Cost function")
+	st.write("Goal is to minimise Cost function where each of the following attributes contribute respective cost.")
+	st.write("Cost function = a(Cumulative Symptomatic) + b(Cumulative Asymptomatic) + c(Cumulative Quarantine)")
+
+	a=st.slider("Cost per unit time of Symptomatic infection",value=13)
+	b=st.slider("Cost per unit time of Asymptomatic infection",value=5)
+	c=st.slider("Cost per unit time of Quarantine",value=2)
+
+	st.write("For better results please change(on the left) the number of times to average the simulation over.")
+	cost_list=[]
+	for i in range(len(hdict['Symptomatic'])):
+		cost=a*hdict['Symptomatic'][i]+b*hdict['Asymptomatic'][i]+c*hdict['Quarantined'][i]
+		cost_list.append(cost)
+
+	fig2, ax2 = plt.subplots()
+	rects = ax2.bar(x, cost_list, width)
+
+	# Add some text for labels, title and custom x-axis tick labels, etc.
+	ax2.set_ylabel('Cost')
+	ax2.set_xlabel('Quarantine degree')
+	ax2.set_title('Effects of different degrees of quarantine on G('+str(n)+','+str(p)+')')
+	ax2.set_xticks(x)
+	ax2.set_xticklabels(labels)
+
+	fig2.tight_layout()
+
+	st.pyplot(fig2)
+
+
 
 #Histogram of cumulative hours(yaxis) vs qdegree(x axis)
 histogram()
-st.write("------------------------------------------------------------------------------------")
 
-st.write("Site under construction...")
+
